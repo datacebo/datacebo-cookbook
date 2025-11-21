b427222f0fbe02590f649287b082c72503f26ea7
Andrew Montanez <andrew@sdv.dev>
2025-11-21 17:07:30 -0600

    Fixing script to automatically make HTML files

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,23 +1,13 @@
 import subprocess
 from pathlib import Path
 
-def _get_modifed_notebooks(mode):
+def _get_modifed_notebooks():
     """Returns a list of paths to modified notebooks.
     
-    Args:
-        mode (str): If `mode` is 'pr', then get the diff of changed notebooks between the PR branch
-        and main. If it is 'commit', then get the diff of changed notebooks between the last two
-        commits.
+    Returns:
+        List of strings that represent paths to notebooks.
     """
-    process_args = ['git', 'diff', '--name-only']
-    if mode == 'pr':
-        process_args.append('origin/main')
-    elif mode == 'commit':
-        process_args.extend(['HEAD~1', 'HEAD'])
-    else:
-        raise ValueError("'mode' must be either 'commit' or 'pr'")
-    
-    process_args.extend(['--', '*.ipynb'])
+    process_args = ['git', 'diff', '--name-only', 'origin/main', '--', '*.ipynb']
     git_diff = subprocess.run(
         process_args,
         capture_output=True,
