5723470ad9be4365e463712c33ae2eb86d45ee24
sarahmish <sarahalnegheimish@gmail.com>
2025-12-08 12:03:05 -0500

    add CSSHTMLHeaderPreprocessor to exporter config

--- scripts/create_notebook_html.py
+++ scripts/create_notebook_html.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 import nbformat
+from traitlets.config import Config
 from nbconvert.exporters import HTMLExporter
 
 
@@ -10,17 +11,19 @@ def get_all_notebook_paths():
     return notebook_paths
 
 def convert_notebooks_to_html(notebook_paths):
+    c = Config()
+    c.CSSHTMLHeaderPreprocessor.style = 'material'
     for notebook_path in notebook_paths:
         parent_folder = notebook_path.parent
         filename = os.path.splitext(os.path.basename(notebook_path))[0]
         html_file_path = parent_folder / f'{filename}.html'
-        if html_file_path.exists():
-            continue
+        # if html_file_path.exists():
+        #     continue
 
         with open(notebook_path, 'r', encoding='utf-8') as f:
             notebook = nbformat.read(f, as_version=4)
 
-        html_exporter = HTMLExporter()
+        html_exporter = HTMLExporter(config=c)
         html_exporter.exclude_input = False
         body, _ = html_exporter.from_notebook_node(notebook)
         with open(html_file_path, 'w', encoding='utf-8') as f:
