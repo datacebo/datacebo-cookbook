50c9b08153eb7024bc4dede99a2606940f76ff92
sarahmish <sarahalnegheimish@gmail.com>
2025-12-08 12:19:05 -0500

    remove comment

--- scripts/create_notebook_html.py
+++ scripts/create_notebook_html.py
@@ -17,8 +17,8 @@ def convert_notebooks_to_html(notebook_paths):
         parent_folder = notebook_path.parent
         filename = os.path.splitext(os.path.basename(notebook_path))[0]
         html_file_path = parent_folder / f'{filename}.html'
-        # if html_file_path.exists():
-        #     continue
+        if html_file_path.exists():
+            continue
 
         with open(notebook_path, 'r', encoding='utf-8') as f:
             notebook = nbformat.read(f, as_version=4)
