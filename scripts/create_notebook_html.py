0895beb8ad2b3e37ef4bd70b32e97a769602b332
sarahmish <sarahalnegheimish@gmail.com>
2026-03-06 17:58:36 +0300

    add recreation workflow

--- scripts/create_notebook_html.py
+++ scripts/create_notebook_html.py
@@ -1,4 +1,6 @@
+import argparse
 import os
+import logging
 from pathlib import Path
 
 import nbformat
@@ -7,11 +9,13 @@ from nbconvert.exporters import HTMLExporter
 
 os.environ['SDMETRICS_PLOTLY_RENDERER'] = 'png'
 
+logger = logging.getLogger(__name__)
+
 def get_all_notebook_paths():
     notebook_paths = list(Path(os.getcwd()).rglob("*.ipynb"))
     return notebook_paths
 
-def convert_notebooks_to_html(notebook_paths):
+def convert_notebooks_to_html(notebook_paths, only_new=False):
     c = Config()
     c.CSSHTMLHeaderPreprocessor.style = 'material'
     c.TemplateExporter.exclude_input_prompt = True
@@ -19,11 +23,14 @@ def convert_notebooks_to_html(notebook_paths):
     c.ExecutePreprocessor.enabled = True
     c.ExecutePreprocessor.timeout = 3600
 
+    docs_dir = Path(os.getcwd()) / 'docs'
     for notebook_path in notebook_paths:
         parent_folder = notebook_path.parent
         filename = os.path.splitext(os.path.basename(notebook_path))[0]
-        html_file_path = parent_folder / f'{filename}.html'
-        if html_file_path.exists():
+        html_docs_path = docs_dir / f'{parent_folder.name}/{filename}.html'
+        html_file_path = parent_folder /f'{filename}.html'
+        if only_new and html_docs_path.exists():
+            logger.info(f'Skipping {filename} because {html_docs_path} already exists.')
             continue
 
         with open(notebook_path, 'r', encoding='utf-8') as f:
@@ -38,5 +45,15 @@ def convert_notebooks_to_html(notebook_paths):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--all',
+        type=bool,
+        default=False,
+        help='Whether to run all notebooks or only the ones that do not already have an HTML file.'
+    )
+    args = parser.parse_args()
+    only_new = not(args.all)
+
     notebook_paths = get_all_notebook_paths()
-    convert_notebooks_to_html(notebook_paths)
+    convert_notebooks_to_html(notebook_paths, only_new=only_new)
